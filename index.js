36cf9819d0d52a1e094e6aeca6145981fbe83274
Damian Malysza <damian.malysza@gmail.com>
2021-10-20 18:32:44 -0400

    commit tree ea9555a63c5feb097fcd8f9e60e1d5ab962d925d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData(name,email){
+  let postRequest = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: `${name}`,
+      email: `${email}`
+    })
+  }
+
+  return fetch("http://localhost:3000/users",postRequest)
+  .then(response => response.json())
+  .then(json => document.body.append(json.id))
+  .catch(error => document.body.append(error.message))
+}
